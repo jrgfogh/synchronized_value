a9f7cd77796cd075805cbddcd890d124874ca515
Jørgen Fogh <jrgfogh@gmail.com>
2024-02-13 02:46:16 +0100

    Example: Finished very simple doubly linked list implementation.

--- examples/doubly_linked_list.h
+++ examples/doubly_linked_list.h
@@ -6,17 +6,16 @@
 template <typename Element>
 class doubly_linked_list
 {
-	friend class iterator;
-
 	struct node_t
 	{
 		Element value;
-		std::unique_ptr<node_t> next_;
-		node_t *prev_;
+		std::unique_ptr<node_t> next;
+		node_t *prev;
 	};
 
 	class iterator
 	{
+		friend class doubly_linked_list;
 		node_t *node_;
 	public:
 		explicit iterator(node_t* node)
@@ -26,8 +25,9 @@ class doubly_linked_list
 
 		bool operator==(iterator const &other) const = default;
 
-		iterator &operator++()
+		iterator const &operator++()
 		{
+			node_ = node_->next.get();
 			return *this;
 		}
 
@@ -38,25 +38,60 @@ class doubly_linked_list
 		}
 	};
 
+	// INVARIANT: !!head_ == !!tail_.
 	std::unique_ptr<node_t> head_;
+	node_t * tail_ = nullptr;
+	// INVARIANT: The list contains exactly size_ nodes.
+	size_t size_ = 0;
+
+	iterator insert_empty(Element const &value)
+	{
+		head_ = std::make_unique<node_t>(value);
+		tail_ = head_.get();
+		return iterator{head_.get()};
+	}
+
+	iterator insert_head(Element const &value)
+	{
+		head_ = std::make_unique<node_t>(value, std::move(head_));
+		head_->next->prev = head_.get();
+		return iterator{head_.get()};
+	}
+
+	iterator insert_tail(Element const &value)
+	{
+		if (!head_)
+			return insert_empty(value);
+		tail_->next = std::make_unique<node_t>(value, nullptr, tail_);
+		tail_ = tail_->next.get();
+		return iterator{tail_};
+	}
 public:
-	iterator insert(iterator pos, Element const& value)
+	iterator insert(iterator pos, Element const &value)
 	{
-		if (pos.node_)
-		{
-		}
-		else
-		{
-		}
+		++size_;
+		if (!pos.node_)
+			return insert_tail(value);
+		if (!pos.node_->prev)
+			return insert_head(value);
+		auto &prev_next = pos.node_->prev->next;
+		prev_next = std::make_unique<node_t>(value, std::move(prev_next), pos.node_->prev);
+		pos.node_->prev = prev_next.get();
+		return iterator{prev_next.get()};
 	}
 
-	auto begin() -> iterator
+	[[nodiscard]] auto begin() const -> iterator
+	{
+		return iterator{head_.get()};
+	}
+
+	[[nodiscard]] auto end() const -> iterator
 	{
 		return iterator{nullptr};
 	}
 
-	auto end() -> iterator
+	[[nodiscard]] size_t size() const
 	{
-		return iterator{nullptr};
+		return size_;
 	}
 };
