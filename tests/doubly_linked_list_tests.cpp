596f71f42e328f37c901ef18a5316d9be387c031
Jørgen Fogh <jrgfogh@gmail.com>
2024-02-17 17:39:16 +0100

    Examples: Refactored doubly_linked_list_tests.cpp to use typed tests.

--- tests/doubly_linked_list_tests.cpp
+++ tests/doubly_linked_list_tests.cpp
@@ -9,21 +9,29 @@
 
 #include "../synchronized_value/synchronized_value.h"
 
-TEST(doubly_linked_list, iterate_empty_list)
+template <typename DoublyLinkedList>
+class doubly_linked_list_tests : public ::testing::Test {
+public:
+};
+
+using list_types = ::testing::Types<doubly_linked_list<int>>;
+TYPED_TEST_SUITE(doubly_linked_list_tests, list_types);
+
+TYPED_TEST(doubly_linked_list_tests, iterate_empty_list)
 {
-	for (const doubly_linked_list<int> l; int _ : l) {}
+	for (const TypeParam l; int _ : l) {}
 }
 
-TEST(doubly_linked_list, insert_element_and_iterate) {
-	doubly_linked_list<int> l;
+TYPED_TEST(doubly_linked_list_tests, insert_element_and_iterate) {
+	TypeParam l;
 	l.insert(l.end(), 1);
 	for (int d : l)
 		EXPECT_EQ(d, 1);
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, insert_three_elements_and_iterate) {
-	doubly_linked_list<int> l;
+TYPED_TEST(doubly_linked_list_tests, insert_three_elements_and_iterate) {
+	TypeParam l;
 	l.insert(l.end(), 1);
 	l.insert(l.end(), 2);
 	l.insert(l.end(), 3);
@@ -34,8 +42,8 @@ TEST(doubly_linked_list, insert_three_elements_and_iterate) {
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, insert_three_elements_in_reverse_and_iterate) {
-	doubly_linked_list<int> l;
+TYPED_TEST(doubly_linked_list_tests, insert_three_elements_in_reverse_and_iterate) {
+	TypeParam l;
 	auto next = l.insert(l.end(), 1);
 	next = l.insert(next , 2);
 	l.insert(next , 3);
@@ -46,8 +54,8 @@ TEST(doubly_linked_list, insert_three_elements_in_reverse_and_iterate) {
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, insert_five_elements_out_of_order_and_iterate) {
-	doubly_linked_list<int> l;
+TYPED_TEST(doubly_linked_list_tests, insert_five_elements_out_of_order_and_iterate) {
+	TypeParam l;
 	auto next = l.insert(l.end(), 1);
 	l.insert(next, 2);
 	next = l.insert(l.end(), 3);
@@ -60,24 +68,24 @@ TEST(doubly_linked_list, insert_five_elements_out_of_order_and_iterate) {
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, insert_and_erase_elements) {
-	doubly_linked_list<double> l;
-	l.insert(l.end(), 1.);
-	doubly_linked_list<double>::iterator next = l.insert(l.end(), 2.);
-	l.insert(l.end(), 3.);
+TYPED_TEST(doubly_linked_list_tests, insert_and_erase_elements) {
+	TypeParam l;
+	l.insert(l.end(), 1);
+	typename TypeParam::iterator next = l.insert(l.end(), 2);
+	l.insert(l.end(), 3);
 	next = l.erase(next);
-	EXPECT_EQ(*next, 3.);
+	EXPECT_EQ(*next, 3);
 	next = l.erase(l.begin());
-	EXPECT_EQ(*next, 3.);
+	EXPECT_EQ(*next, 3);
 	next = l.erase(l.begin());
 	EXPECT_EQ(next, l.end());
-	l.insert(l.end(), 5.);
+	l.insert(l.end(), 5);
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, stress_test) {
-	doubly_linked_list<int> l;
-	std::vector<doubly_linked_list<int>::iterator> iterators;
+TYPED_TEST(doubly_linked_list_tests, stress_test) {
+	TypeParam l;
+	std::vector<typename TypeParam::iterator> iterators;
 	iterators.reserve(5000);
 	for (int i = 0; i < 5000; i++)
 		iterators.emplace_back(l.insert(l.end(), i));
@@ -89,7 +97,7 @@ TEST(doubly_linked_list, stress_test) {
 	EXPECT_TRUE(l.check_invariants());
 }
 
-TEST(doubly_linked_list, async) {
+TEST(doubly_linked_list_tests, async) {
 	doubly_linked_list<int> l;
 	std::vector<doubly_linked_list<int>::iterator> iterators;
 	iterators.reserve(5000);
