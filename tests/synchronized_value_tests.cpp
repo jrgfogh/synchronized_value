d9fd2a1113f39396ee43b0ad4bd6401a00aa68b5
Clang Robot <robot@example.com>
2024-04-25 21:13:42 +0000

    Committing clang-format changes

--- tests/synchronized_value_tests.cpp
+++ tests/synchronized_value_tests.cpp
@@ -7,23 +7,20 @@ static_assert(std::is_nothrow_move_constructible_v<update_guard<int>>);
 static_assert(std::is_nothrow_swappable_v<update_guard<int>>);
 
 TEST(synchronized_value_tests, star_operator) {
-	synchronized_value<int> sv{5};
-	EXPECT_EQ(*sv, 5);
-	*sv = 2;
-	EXPECT_EQ(*sv, 2);
+  synchronized_value<int> sv{5};
+  EXPECT_EQ(*sv, 5);
+  *sv = 2;
+  EXPECT_EQ(*sv, 2);
 }
 
 TEST(synchronized_value_tests, BasicAssertions) {
-	synchronized_value<std::map<int, int>> sv0;
-	sv0->emplace(0, 5);
-	{
-		update_guard guard{sv0};
-		++(*guard)[0];
-		auto var = guard->at(0);
-	}
-	synchronized_value<std::tuple<int, int, int>> sv1{1,33,7};
-	sv1.apply([](auto tuple)
-	{
-		auto [a, b, c] = tuple;
-	});
+  synchronized_value<std::map<int, int>> sv0;
+  sv0->emplace(0, 5);
+  {
+    update_guard guard{sv0};
+    ++(*guard)[0];
+    auto var = guard->at(0);
+  }
+  synchronized_value<std::tuple<int, int, int>> sv1{1, 33, 7};
+  sv1.apply([](auto tuple) { auto [a, b, c] = tuple; });
 }
