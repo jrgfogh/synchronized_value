e689cd53d531da728242a2c5b851c8bf0b28a14a
Jørgen Fogh <jrgfogh@gmail.com>
2024-05-04 11:31:09 +0200

    Added synchronized_stack example.

---
+++ examples/synchronized_stack.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../synchronized_value/synchronized.h"
+
+#include <optional>
+#include <stack>
+
+namespace example {
+/// A simple example to show the basic usage of `synchronized`.
+template <typename Element>
+class synchronized_stack {
+  sv::synchronized<std::stack<int>> data_;
+
+public:
+  using value_type = Element;
+
+  void push(int d) { data_.wlock()->push(d); }
+  std::optional<int> pop() {
+    auto lock = data_.wlock();
+    if (lock->empty()) return {};
+    auto result = lock->top();
+    lock->pop();
+    return result;
+  }
+  auto empty() { return data_.rlock()->empty(); }
+  auto size() { return data_.rlock()->size(); }
+};
+} // namespace example
